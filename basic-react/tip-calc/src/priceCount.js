6943bdd829bf230a4bae4acacf4520ac8d58c545
wayne <chengzhou.weng@fraudmetrix.cn>
2017-10-29 12:15:59 +0800

    holt shit

--- basic-react/tip-calc/src/priceCount.js
+++ basic-react/tip-calc/src/priceCount.js
@@ -3,18 +3,14 @@ import React, { Component } from 'react'
 class priceCount extends Component {
   constructor(props) {
     super(props)
-  }
-
-  handleInput(e) {
-		this.setState({price: e.target.value})
-  }
-
+	}
+	
   render() {
     const { count, price } = this.props
 
     return (
       <div className="xxxx">
-        <input readOnly type="number" value={price} onChange={this.handleInput.bind(this)} />
+        <input readOnly type="number" value={price}/>
         <span>{count * 100 + '% = ' + `${(price * count).toFixed(2)}`}</span>
       </div>
     )
